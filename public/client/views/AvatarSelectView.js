46c21e660ee302d97f2cda0729ff6da3908018ab
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-04 03:33:35 +0000

    Add avatar image list

--- public/client/views/AvatarSelectView.js
+++ public/client/views/AvatarSelectView.js
@@ -1,11 +1,14 @@
-var avatars = ['A01', 'A02', 'A03', 'A04', 'A05'];
+var avatars = [
+  {val: 'A01'}, {val: 'A02'}, {val: 'A03'}, {val: 'A04'}, {val: 'A05'}, {val: 'B01'},
+  {val: 'FA01'}, {val: 'FA02'}, {val: 'FA03'}, {val: 'FA04'}, {val: 'FA05'}, {val: 'FC01'}
+];
 
 var AvatarSelectView = Backbone.View.extend({
   id: 'avatars',
 
   template: _.template('<label class="thumb">'+
-                        '<input type="radio" name="avatar" value="">'+
-                        '<img src="client/css/images/avatars/A01.png" />'+
+                        '<input type="radio" name="avatar" value="<%=val%>">'+
+                        '<img src="client/css/images/avatars/<%=val%>.png" />'+
                        '</label>'
                        ),
 
