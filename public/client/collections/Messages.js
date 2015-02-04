5d47187adda2014d474045427efd3568019e9662
Clark Feusier <cfeusier@gmail.com>
2015-02-03 16:50:36 -0800

    Refactor messages collection

--- public/client/collections/Messages.js
+++ public/client/collections/Messages.js
@@ -5,14 +5,13 @@ var Messages = Backbone.Collection.extend({
   //instantiate new model
   //add model to collection
   addmsg: function(msg){
-    var message = new Message({
+    this.add(new Message({
       username: msg.username,
       text: msg.text,
       lang: msg.lang,
       room: msg.room,
       translations: msg.translations
-    });
-    this.add(message);
+    }));
   }
 
 });
