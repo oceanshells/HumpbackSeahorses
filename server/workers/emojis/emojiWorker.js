5f838511d768d4e883a0ee3b88a4247e0b5a1069
Clark Feusier <cfeusier@gmail.com>
2015-02-03 22:00:05 -0800

    Setup package.json with extra gulp dependencies

---
+++ server/workers/emojis/emojiWorker.js
@@ -0,0 +1,22 @@
+var request = require('request');
+var Emoji = require('../../../db/models/Emoji.js');
+
+var worker = {
+  getUrls: function(){
+
+  },
+
+  getImgData: function(){
+
+  },
+
+  saveImgs: function(){
+
+  },
+
+  saveImg: function(){
+
+  }
+};
+
+module.exports = worker;
