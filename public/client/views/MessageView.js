64aeeced0a8d3d5a759f5f06bd7a372132701b67
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-05 20:39:01 +0000

    Initial display of original language

--- public/client/views/MessageView.js
+++ public/client/views/MessageView.js
@@ -1,13 +1,18 @@
 //returns rendered template
 var MessageView = Backbone.View.extend({
+  className: 'message-display',
+
   template : _.template(
-    '<div class="message-display">'+
-      '<span>'+
-        '<img src="client/images/avatars/<%- avatar %>.png">'+ 
-        '<strong><%- username %></strong>@<%- room %> - <%- text %>'+
-      '</span>'+
-    '</div>'
+    '<span>'+
+      '<span class="badge lang-badge pull-right">orig</span>'+
+      '<img src="client/images/avatars/<%- avatar %>.png">'+ 
+      '<strong><%- username %></strong>@<%- room %> - <span style="display: none;" class="alt-text"><%- translations[lang] %></span><span class="text"><%- text %></span>'+
+    '</span>'
     ),
+
+  events: {
+    'click span.badge': 'toggleLanguage'
+  },
 
   render:function(){
     this.$el.html(this.template(this.model.attributes));
@@ -15,6 +20,12 @@ var MessageView = Backbone.View.extend({
     imSnd.play();
     return this.$el;
   },
+
+  toggleLanguage: function(e) {
+    console.log('click!');
+    this.$el.find('span.text').toggle();
+    this.$el.find('span.alt-text').toggle();
+  }
 });
 
 
