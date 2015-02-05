e597beb2eb75ee8b93e33108907f46266a58e58f
Omar Alvarez <omar.alvarez2006@gmail.com>
2015-02-05 01:28:37 +0000

    Remove unused intialize method

--- public/client/views/MessageView.js
+++ public/client/views/MessageView.js
@@ -9,9 +9,6 @@ var MessageView = Backbone.View.extend({
     '</div>'
     ),
 
-  initialize: function() {
-  },
-
   render:function(){
     this.$el.html(this.template(this.model.attributes));
     var imSnd = new Audio('client/sounds/im.wav');
